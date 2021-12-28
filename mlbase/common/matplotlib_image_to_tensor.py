b02ee407880b70879f68a79b51573658a6dcee33
Paulo Soares <paulosoares@SBS-7186.local>
2021-12-28 11:23:52 -0700

    Include return type to function

--- mlbase/common/matplotlib_image_to_tensor.py
+++ mlbase/common/matplotlib_image_to_tensor.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import io
 import PIL
 from torchvision.transforms import ToTensor
+import torch
 
 
-def matplotlib_image_to_tensor(figure: plt.figure):
+def matplotlib_image_to_tensor(figure: plt.figure) -> torch.tensor:
     buffer = io.BytesIO()
     plt.savefig(buffer, format='png')
     plt.close(figure)
